cce487cf32e16b48ccf6746e1845ff7c5a845bf2
boudewijnbout <boudewijn_bout@outlook.com>
2022-09-06 16:20:43 +0200

    Added basic card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,38 @@
 </script>
 
 <main>
-    <h1>{data.document.data.title}</h1>
 
-    {@html prismicH.asHTML(data.document.data.content)}
-
-    <img src="{data.document.data.image.url}" alt="">
+    <section class="personal-card">
+        <h1>{data.document.data.name[0].text}</h1>
+        {@html prismicH.asHTML(data.document.data.content)}
+        <img src="{data.document.data.image.url}" alt="">
+    </section>
 </main>
+
+<style>
+    :global(body) {
+        background-color: #f1f1f1f1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    .personal-card {
+        background-color: white;
+        width: 25vw;
+        transform: rotateX(60deg) rotateY(0deg) rotateZ(45deg);
+        padding: 1rem 1rem;
+        border-radius: 10px;
+        transition: 200ms;
+    }
+
+    .personal-card:hover {
+        transform: none;
+    }
+
+    .personal-card img {
+        width: 50%;
+        height: auto;
+    }
+</style>
